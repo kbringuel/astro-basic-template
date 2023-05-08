0b58b75db1b69f56e85fe61e5984fed292fce62e
Brody Over <brodyover@gmail.com>
2023-05-08 11:14:01 -0400

    listen on host 0.0.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,9 @@ export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000
+  }
 });
