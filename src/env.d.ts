470e50f76759388002f7c4afaaa48bfed0c4c184
kbringuel <68448750+kbringuel@users.noreply.github.com>
2023-05-22 17:53:07 -0700

    url

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
-/// <reference types="astro/client" />
+PUBLIC_DIRECTUS_URL=https://directus-production-fb6d.up.railway.app
+DIRECTUS_EMAIL=admin@example.com
+DIRECTUS_PASSWORD=d1r3ctu5
+DIRECTUS_STATIC_TOKEN=
